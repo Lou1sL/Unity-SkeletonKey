0290961d58df5814e105eaecebcf92b44e009fd7
留白 <me@ryubai.com>
2018-10-09 09:38:54 +0800

    。。。

--- Payload/MonoScript/Tetris.cs
+++ Payload/MonoScript/Tetris.cs
@@ -8,15 +8,61 @@ namespace Payload.MonoScript
 {
     public class Tetris : MonoBehaviour
     {
+        public class Graph
+        {
+            private List<List<bool>> graph;
+            public Graph(int row,int column)
+            {
+                Init(row, column);
+            }
+            public void Init(int row, int column)
+            {
+                graph = new List<List<bool>>(row);
+                for (int i = 0; i < row; i++) graph[i] = new List<bool>(column) { false };
+            }
+            public void Clear()
+            {
+                for (int i = 0; i < graph.Count; i++)
+                {
+                    for(int j = 0; j < graph[i].Count; j++)
+                    {
+                        graph[i][j] = false;
+                    }
+                }
+            }
+            public bool GetPoint(int x, int y)
+            {
+                if (x >= graph.Count || y >= graph[0].Count || x < 0 || y < 0) return false;
+                return graph[x][y];
+            }
+            public void SetPoint(int x,int y,bool b)
+            {
+                if (x >= graph.Count || y >= graph[0].Count || x < 0 || y < 0) return;
+                graph[x][y] = b;
+            }
+        }
+
+        private Graph graph = new Graph(15, 35);
         private KeyCode PlayTetris = KeyCode.Pause;
-        private bool PlayingTetris = true;
+        public bool Activited { get; private set; }
+        public void Active()
+        {
+            Activited = true;
+            graph.Clear();
+        }
+        public void DeActive()
+        {
+            Activited = false;
+            graph.Clear();
+        }
+
         private void Update()
         {
-            PlayingTetris = Input.GetKeyDown(PlayTetris) ? (!PlayingTetris) : PlayingTetris;
+            
         }
         private void OnGUI()
         {
-            if (PlayingTetris) Game();
+            
         }
 
         private void Game()
