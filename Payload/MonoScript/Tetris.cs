9ed5e908275a030154a5ca21788210f4f1a752ee
留白 <156890093@qq.com>
2018-11-04 18:31:19 +0800

    Update Payload/MonoScript/Tetris.cs

--- Payload/MonoScript/Tetris.cs
+++ Payload/MonoScript/Tetris.cs
@@ -8,38 +8,139 @@ namespace Payload.MonoScript
 {
     public class Tetris : MonoBehaviour
     {
-        public class Graph
+
+        public static class Shape
         {
-            private List<List<bool>> graph;
-            public Graph(int row,int column)
+            public enum ShapeType
             {
-                Init(row, column);
+                S, Z, T, L, Line, MirroredL, Square
             }
-            public void Init(int row, int column)
+
+            public static int[,] GetnerateShape(ShapeType type)
             {
-                graph = new List<List<bool>>(row);
-                for (int i = 0; i < row; i++) graph[i] = new List<bool>(column) { false };
-            }
-            public void Clear()
-            {
-                for (int i = 0; i < graph.Count; i++)
+                switch (type)
                 {
-                    for(int j = 0; j < graph[i].Count; j++)
-                    {
-                        graph[i][j] = false;
-                    }
+                    case ShapeType.S:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+
+                    case ShapeType.Z:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+                    case ShapeType.T:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+                    case ShapeType.L:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+                    case ShapeType.Line:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+                    case ShapeType.MirroredL:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+                    case ShapeType.Square:
+                        return new int[4, 4]
+                        {
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                            { 0, 0, 0, 0 },
+                        };
+                    default:return null;
                 }
             }
-            public bool GetPoint(int x, int y)
+
+        }
+
+
+        public class Graph
+        {
+
+
+            //标准俄罗斯方块棋盘大小：10x24
+            private int[,] graph = new int[24,10];
+            public int Row { get; private set; } = 24;
+            public int Column { get; private set; } = 10;
+
+            private int[,] mvobj = new int[4, 4];
+
+            public Graph(int row = 24,int column = 10)
             {
-                if (x >= graph.Count || y >= graph[0].Count || x < 0 || y < 0) return false;
-                return graph[x][y];
+                Row = row;Column = column;
+                graph = new int[row, column];
             }
-            public void SetPoint(int x,int y,bool b)
+            public int GetPoint(int x, int y)
             {
-                if (x >= graph.Count || y >= graph[0].Count || x < 0 || y < 0) return;
-                graph[x][y] = b;
+                if (x >= Row || y >= Column || x < 0 || y < 0) return -1;
+                return graph[x, y];
             }
+            //逻辑帧
+            public void UpdateLogic()
+            {
+                RowClearChk();
+
+
+            }
+
+
+
+            public void Clear()
+            {
+                for (int i = 0; i < Row; i++)
+                    for (int j = 0; j < Column; j++)
+                        graph[i, j] = 0;
+            }
+            
+            private void SetPoint(int x,int y,int value)
+            {
+                if (x >= Row || y >= Column || x < 0 || y < 0) return;
+                graph[x,y] = value;
+            }
+            private void RowClearChk()
+            {
+                bool clear = true;
+
+                for (int j = 0; j < Column; j++) if (graph[Row - 1, j] > 0)
+                        clear = false;
+
+                if (clear)
+                    for (int i = 1; i < Row; i++)
+                        for (int j = 0; j < Column; j++)
+                            graph[i, j] = graph[i - 1, j];
+            }
+
+            
         }
 
         private Graph graph = new Graph(15, 35);
@@ -56,11 +157,9 @@ namespace Payload.MonoScript
         }
         private void OnGUI()
         {
-            
-        }
 
-        private void Game()
-        {
+            GUI.DrawTexture(new Rect(10, 10, 10, 10), new Texture());
+
 
         }
     }
